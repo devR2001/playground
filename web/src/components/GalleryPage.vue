0d173e5ea324167b555a6c6b9a8b0b4e4295d2ef
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-12 23:50:37 +0200

    dev

--- web/src/components/GalleryPage.vue
+++ web/src/components/GalleryPage.vue
@@ -5,21 +5,36 @@
       Type a number: <input v-model.number="number" />
       <p class="big-number">{{ tweened.number.toFixed(0) }}</p>
     </div>
+
+    <p>Title: {{ list.title }}</p>
+    <p>Body: {{ list.body }}</p>
   </template>
   
   <script setup>
-  import { ref, reactive, watch } from 'vue'
+  import { ref, reactive, watch, onMounted } from 'vue'
   import gsap from 'gsap'
   
+  const list = ref([])
   const number = ref(0)
   const tweened = reactive({
     number: 0
   })
+
+  const fetchData = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const data = await response.json()
+    list.value = data
+    console.log(data)
+  }
   
   watch(number, (n) => {
       gsap.to(tweened, { duration: 1, number: Number(n) || 0 })
     }
   )
+
+onMounted(() => {
+    fetchData();
+});
   </script>
   
   <style>
